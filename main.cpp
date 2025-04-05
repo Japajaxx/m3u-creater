87b45b9b822e36963fc49f9352108207d0f36d6c
George Hornseth <gbhornseth@gmail.com>
2025-04-04 19:41:10 -0500

    Made example for testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-
 #include <iostream>
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
+
+    fs::path filePath = "example.txt";
+
+    cout << "The destination of the file is: " << fs::absolute(filePath) << endl;
 
     return 0;
 }
